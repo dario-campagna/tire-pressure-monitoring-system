231fc298e750a83794cff8d56a2c2d2ae2b96064
Dario Campagna <campagna@esteco.com>
2020-11-28 17:45:26 +0100

    Add tests with stubs

--- src/test/java/dssc/example/AlarmTest.java
+++ src/test/java/dssc/example/AlarmTest.java
@@ -2,14 +2,43 @@ package dssc.example;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AlarmTest {
 
     @Test
-    void alarm_could_be_on() {
-        Alarm alarm = new Alarm(new SimulatedSensor());
+    void pressureWithinThresholds() {
+        Alarm alarm = new Alarm(new StubSensor(19));
+        alarm.check();
+        assertFalse(alarm.isAlarmOn());
+    }
+
+    @Test
+    void lowPressure() {
+        Alarm alarm = new Alarm(() -> 10);
         alarm.check();
         assertTrue(alarm.isAlarmOn());
+    }
+
+    @Test
+    void highPressure() {
+        Alarm alarm = new Alarm(() -> 30);
+        alarm.check();
+        assertTrue(alarm.isAlarmOn());
+    }
+
+    public class StubSensor implements Sensor {
+
+        private double psiValue;
+
+        public StubSensor(double psiValue) {
+            this.psiValue = psiValue;
+        }
+
+        @Override
+        public double popNextPressurePsiValue() {
+            return psiValue;
+        }
     }
 }
